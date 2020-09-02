f0eb02eac45a36f185153ecd526d28b48ce758de
Anis Da Silva Campos <anis@lumapps.com>
2020-09-02 15:43:20 +0200

    chore(pre-commit): allow fixups and revert
    Now we can fixup locally.
    The github action should still block the merge though

--- check_message.sh
+++ check_message.sh
@@ -2,17 +2,29 @@
 
 set -eu
 
-if [[ -v ZSH_NAME ]]; then
-  setopt BASH_REMATCH
-  setopt RE_MATCH_PCRE
-  setopt KSH_ARRAYS
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# shellcheck source=validator.sh
+source "$DIR/validator.sh"
+
+
+if [[ "$1" == *MERGE_MSG ]]
+then
+  # ignore merge message (merge with --no-ff without conflict)
+  exit
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $DIR/validator.sh
-
 MESSAGE=$(<"$1")
-echo "checking commit message: '${MESSAGE}'"
-validate "$MESSAGE"
 
-echo "Message succesfully checked"
+FIRST_WORD=${MESSAGE%% *}
+if [[ "${FIRST_WORD,,}" == merge ]]
+then
+   # ignore merge commits (merge after conflict resolution)
+  exit
+
+fi
+
+# print message so you don't lose it in case of errors
+# (in case you are not using `-m` option)
+printf "checking commit message:\n\n#BEGIN#\n%s\n#END#\n\n" "$(grep -v "#" <<< "$MESSAGE")"
+
+COMMIT_VALIDATOR_ALLOW_TEMP=1 COMMIT_VALIDATOR_NO_JIRA=1 validate "$MESSAGE"
