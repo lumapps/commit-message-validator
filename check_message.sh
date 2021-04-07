lumapps/commit-message-validator

commit 0579955c2d12d2522e4bb109585906795fcb52f4

feat(pre-commit): adding new options to the pre-commit shell script
Also making the whole thing posix when handling arguments as not being
able to put = between args and values was frustrating

check_message.sh
@@ -2,21 +2,19 @@
 
 set -eu
 
-OPTIONS=$(getopt --long no-jira allow-temp -- "$@")
-[ $? -eq 0 ] || {
-    echo "Incorrect options provided"
-    exit 1
-}
+OPTIONS=$(getopt --long no-jira,allow-temp,jira-in-header,header-length:,jira-types: -- "$@")
 
-COMMIT_VALIDATOR_ALLOW_TEMP=
-COMMIT_VALIDATOR_NO_JIRA=
-COMMIT_VALIDATOR_NO_REVERT_SHA1=
+unset COMMIT_VALIDATOR_ALLOW_TEMP COMMIT_VALIDATOR_NO_JIRA COMMIT_VALIDATOR_NO_REVERT_SHA1 GLOBAL_JIRA_IN_HEADER GLOBAL_MAX_LENGTH GLOBAL_JIRA_TYPES
 
+eval set -- $OPTIONS
 while true; do
   case "$1" in
     --no-jira ) COMMIT_VALIDATOR_NO_JIRA=1; shift ;;
     --allow-temp ) COMMIT_VALIDATOR_ALLOW_TEMP=1; shift ;;
     --no-revert-sha1 ) COMMIT_VALIDATOR_NO_REVERT_SHA1=1; shift ;;
+    --jira-in-header ) GLOBAL_JIRA_IN_HEADER=1; shift ;;
+    --header-length ) GLOBAL_MAX_LENGTH="$2"; shift 2 ;;
+    --jira-types ) GLOBAL_JIRA_TYPES="$2"; shift 2 ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -47,9 +45,9 @@ fi
 # print message so you don't lose it in case of errors
 # (in case you are not using `-m` option)
 echo "Options: "
-echo "  JIRA=$COMMIT_VALIDATOR_NO_JIRA"
-echo "  TEMP=$COMMIT_VALIDATOR_ALLOW_TEMP"
-echo "  NO_REVERT_SHA1=$COMMIT_VALIDATOR_NO_REVERT_SHA1"
+echo "  JIRA=${COMMIT_VALIDATOR_NO_JIRA:-}"
+echo "  TEMP=${COMMIT_VALIDATOR_ALLOW_TEMP:-}"
+echo "  NO_REVERT_SHA1=${COMMIT_VALIDATOR_NO_REVERT_SHA1:-}"
 printf "checking commit message:\n\n#BEGIN#\n%s\n#END#\n\n" "$MESSAGE"
 
 validate "$MESSAGE"

commit 93f498bdc7aebca2cd6e855b1b35744f5ff6c3bf

fix(check-message): can't run the commit message validator
the error:
```
sed: no-jira,allow-temp,jira-in-header,header-length:,jira-types:: No such file or directory
```
issue introduced in 0579955c2d12d2522e4bb109585906795fcb52f4

check_message.sh
@@ -6,6 +6,7 @@ OPTIONS=$(getopt --long no-jira,allow-temp,jira-in-header,header-length:,jira-ty
 
 unset COMMIT_VALIDATOR_ALLOW_TEMP COMMIT_VALIDATOR_NO_JIRA COMMIT_VALIDATOR_NO_REVERT_SHA1 GLOBAL_JIRA_IN_HEADER GLOBAL_MAX_LENGTH GLOBAL_JIRA_TYPES
 
+# this edits the currents args
 eval set -- $OPTIONS
 while true; do
   case "$1" in
@@ -24,15 +25,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck source=validator.sh
 source "$DIR/validator.sh"
 
-
-if [[ "$1" == *MERGE_MSG ]]
+# usually this is the first arg, now it's forth. See OPTIONS
+MESSAGE_FILE=$4
+if [[ "$MESSAGE_FILE" == *MERGE_MSG ]]
 then
   # ignore merge message (merge with --no-ff without conflict)
   exit
 fi
 
 # removing comment lines from message
-MESSAGE=$(sed '/^#/d' "$1")
+MESSAGE=$(sed '/^#/d' "$MESSAGE_FILE")
 
 FIRST_WORD=${MESSAGE%% *}
 if [[ "${FIRST_WORD,,}" == merge ]]