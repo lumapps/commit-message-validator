ca111b19b18d3397c9e1ebebbc8556e8c43e20dc
Nicolas Noirbent <nicolas.noirbent@lumapps.com>
2026-06-03 12:00:43 +0200

    chore(validator): pipe commit list into loop instead of using temp variables

--- check.sh
+++ check.sh
@@ -5,13 +5,12 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/validator.sh
 
-COMMITS=`git log --no-merges --pretty="%H" --no-decorate $1`
-
+git log --no-merges --pretty="%H" --no-decorate $1 |
 while IFS= read -r COMMIT
 do
    MESSAGE=`git log -1 --pretty='%B' $COMMIT`
    echo "checking commit ${COMMIT}..."
    validate "$MESSAGE"
-done <<< $COMMITS
+done
 
 echo "All commits successfully checked"
